c7a5030388dee5307802b19386f67ef83918f1ed
kavin033 <rk.kavin2003@gmail.com>
2022-06-27 22:04:57 +0530

    file locations

--- Server_arch/arch_headquarters.php
+++ Server_arch/arch_headquarters.php
@@ -4,7 +4,6 @@ include '../load.php';
 echo "Server_arch\n";
 global $__site_config;
 $__site_config = file_get_contents($file."/Server_arch/Headquarters.json");
-echo $file."/Server_arch/Headquarters.json";
 function get_config()
 {
     global $__site_config;
